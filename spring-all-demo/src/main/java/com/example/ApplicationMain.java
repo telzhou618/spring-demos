7f35edc5204c135490b75ca195b2c72eeeac437d
telzhou618 <telzhou618@qq.com>
2021-01-02 12:22:29 +0800

    review

--- spring-all-demo/src/main/java/com/example/ApplicationMain.java
+++ spring-all-demo/src/main/java/com/example/ApplicationMain.java
@@ -1,15 +1,10 @@
 package com.example;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 /**
  * @author jameszhou
  */
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.example");
-      //  System.out.println(context.getBean(UserMapper.class));
     }
 }
